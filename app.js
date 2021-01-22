c16ab6ebcb9194e1b6b84eb63aaeb0911868fa0c
sarajuddin <sarajkazimi@gmail.com>
2021-01-21 22:17:07 -0800

    some changes

--- app.js
+++ app.js
@@ -149,15 +149,15 @@ app.get("/:id", (req, res) => {
 // const subprocess = runScript()
 
 // print output of script
-subprocess.stdout.on("data", (data) => {
-  console.log(`data:${data}`);
-});
-subprocess.stderr.on("data", (data) => {
-  console.log(`error:${data}`);
-});
-subprocess.stderr.on("close", () => {
-  console.log("Closed");
-});
+// subprocess.stdout.on("data", (data) => {
+//   console.log(`data:${data}`);
+// });
+// subprocess.stderr.on("data", (data) => {
+//   console.log(`error:${data}`);
+// });
+// subprocess.stderr.on("close", () => {
+//   console.log("Closed");
+// });
 
 server.listen(PORT, host, function () {
   console.log(`Server is listening on port ${PORT}`);
